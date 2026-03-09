a7a2a5581560e0ea1358771660146b4828431b0d
Jeferson202121 <78312827+Jeferson202121@users.noreply.github.com>
2026-03-09 10:49:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const cacheName = 'bjj-flow-v5';
+const CACHE_NAME = 'bjj-flow-v1';
 const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
